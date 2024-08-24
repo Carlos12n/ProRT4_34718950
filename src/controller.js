844107216249b6610320945fe6d8104c6ed31ddd
navarro754 <navarro754@gmail.com>
2024-08-24 19:57:34 -0300

    Se agrega consulta e insertar un libro

--- src/controller.js
+++ src/controller.js
@@ -1,9 +1,18 @@
 import { pool } from "./database.js";
 
 class LibrosController{
+    //consulta de libros
     async getAll(req, res) {
-        const resultado = await pool.query('SELECT * FROM libro');
+        const [result] = await pool.query('SELECT * FROM libros');
         res.json(result);
+    }
+
+    //creacion de libro
+    async add(req, res) {
+        const libro = req.body;
+        const [result] = await pool.query(`INSERT INTO Libros(nombre, autor, categoria, aniopublicacion, ISBN) VALUES (?,?,?,?,?)`,[libro.nombre, libro.autor, libro.categoria, libro.aniopublicacion, libro.ISBN]);
+        res.json({"Id insertado": result.insertId});
+        
     }
 }
 
